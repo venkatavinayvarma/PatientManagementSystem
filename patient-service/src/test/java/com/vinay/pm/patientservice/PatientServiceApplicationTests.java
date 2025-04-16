f8aaebdf5e7e02b3deb820fc13acb42f7948137b
Vinay Varma <vinayvarma5208@gmail.com>
2025-04-16 17:17:06 -0400

    basic structure

---
+++ patient-service/src/test/java/com/vinay/pm/patientservice/PatientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinay.pm.patientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

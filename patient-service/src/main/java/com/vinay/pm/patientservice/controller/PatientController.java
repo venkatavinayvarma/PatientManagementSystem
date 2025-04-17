70f655a07fa3dae99e9368b82a6496f2abb29df4
Vinay Varma <vinayvarma5208@gmail.com>
2025-04-17 11:12:29 -0400

    coded till calling api get request

---
+++ patient-service/src/main/java/com/vinay/pm/patientservice/controller/PatientController.java
@@ -0,0 +1,32 @@
+package com.vinay.pm.patientservice.controller;
+
+import com.vinay.pm.patientservice.dto.PatientResponseDTO;
+import com.vinay.pm.patientservice.service.PatientService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/patients")
+
+public class PatientController {
+
+    private final PatientService patientService;
+
+
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PatientResponseDTO>> getPatients() {
+        List<PatientResponseDTO> patients = patientService.getPatients();
+        return ResponseEntity.ok().body(patients);
+
+
+    }
+}
